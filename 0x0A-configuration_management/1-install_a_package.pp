15db2b0c3deb6015e47443463fc3b0f44b58d73c
Babatunde <babatundeojomu2016@gmail.com>
2024-04-24 17:26:12 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask using pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+  subscribe   => Package['python3-pip'],
 }
